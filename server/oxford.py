6b2fd5664a66d28eb848e11c974c304e27257fff
Joel Gustafson <joelg@mit.edu>
2016-02-06 16:50:00 -0500

    randomize selections

--- server/oxford.py
+++ server/oxford.py
@@ -1,9 +1,11 @@
 import requests
+import random
 from keys import oxford
 from syllables import count_syllables_word
 
-def get_candidates(string, n=4, model='body'):
-    print(string)
+def get_candidates(string, n=4, model=random.choice(['body', 'title', 'anchor'])):
+    print model
+    print string
     params = {'words': string, 'model': model, 'maxNumOfCandidatesReturned': n}
     headers = {'Ocp-Apim-Subscription-Key': oxford}
     url = 'https://api.projectoxford.ai/text/weblm/v1.0/generateNextWords'
@@ -19,7 +21,8 @@ def get_candidates_in_range(string, max_syllables):
     # here each candidate is a list of tuples of (word, syllable_count)
     candidates = []
     r = get_candidates(string, 20)
-    r.sort(key=lambda c: c['probability'])
+    # r.sort(key=lambda c: c['probability'])
+    random.shuffle(r)
     for candidate in r:
         s = count_syllables_word(candidate['word'])
         if s <= max_syllables:
