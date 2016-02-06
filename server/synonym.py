e1ab0024145eea046bb2475ff27e615a05de1af3
rtran9 <rtranare@gmail.com>
2016-02-06 17:20:05 -0500

    added synonym finder

--- server/synonym.py
+++ server/synonym.py
@@ -1,6 +1,20 @@
 import nltk
 nltk.data.path.append("./nltk_data")
 
-database = nltk.corpus.wordnet.synset('dog.n.01')
+database = nltk.corpus.wordnet
 
-print database.name()
+#assume word is valid and all lowerecase
+def get_synonyms(word):
+    words = []
+    synonyms = database.synsets(word)
+    for elt in synonyms:
+        names = elt.lemma_names()
+        for name in names:
+            name = str(name)
+            #replace hyphens with a space
+            name = name.replace("-", " ")
+            if name not in words:
+                words.append(name)
+    #sometimes it will return the same word as a synonym, remove from list to be safe
+    words.remove(word)
+    return words
