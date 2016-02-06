9e0fe0cb4fab898c940a1f88f1a03aee64a6f48e
Joel Gustafson <joelg@mit.edu>
2016-02-06 17:25:19 -0500

    bugfixes

--- server/preview.py
+++ server/preview.py
@@ -1,9 +1,11 @@
 from oxford import get_candidates, get_candidates_in_range
+from syllables import count_syllables_haiku
+
+# here candidates are list of tuples of (word, syllable_count)
+def candidate_to_string(candidate):
+    return ' '.join([c[0] for c in candidate])
 
 def complete_range(string, max_syllables):
-    # here candidates are list of tuples of (word, syllable_count)
-    def candidate_to_string(candidate):
-        return string + ' ' + ' '.join([c[0] for c in candidate])
 
     completion = []
 
@@ -15,13 +17,36 @@ def complete_range(string, max_syllables):
         s = sum([c[1] for c in candidate])
         if s == max_syllables:
             return candidate
-        next_candidates = get_candidates_in_range(candidate_to_string(candidate), max_syllables - s)
+        next_candidates = get_candidates_in_range(string + ' ' + candidate_to_string(candidate), max_syllables - s)
         queue += [candidate + [c] for c in next_candidates]
 
     print 'could not complete'
 
 def complete_haiku(haiku):
-    
-    pass
+    # haiku is a list of strings
+    haiku_count = count_syllables_haiku(haiku)
 
-print complete_range('my', 13)
+    # first line
+    max_syllables = 5 - haiku_count[0]
+    if max_syllables > 0:
+        haiku[0] = candidate_to_string(complete_range(haiku[0], max_syllables))
+    elif max_syllables < 0:
+        print('this is bad')
+
+    # second line
+    max_syllables = 7 - haiku_count[1]
+    if max_syllables > 0:
+        haiku[1] = candidate_to_string(complete_range(haiku[0] + ' ' + haiku[1], max_syllables))
+    elif max_syllables < 0:
+        print('this is bad')
+
+    # third line
+    max_syllables = 5 - haiku_count[2]
+    if max_syllables > 0:
+        haiku[2] = candidate_to_string(complete_range(haiku[0] + ' ' + haiku[1] + ' ' + haiku[2], max_syllables))
+    elif max_syllables < 0:
+        print('this is bad')
+
+    return haiku
+
+print complete_haiku(['yesterday I ate', 'a', ''])
