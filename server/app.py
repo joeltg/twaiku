ebf7d99ff77734e8fa1d7a7cd7c55f1ec4c6746c
rtran9 <rtranare@gmail.com>
2016-02-07 02:37:37 -0500

    websocket fake data

--- server/app.py
+++ server/app.py
@@ -2,11 +2,18 @@ from flask import Flask
 from flask_sockets import Sockets
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
+import json
+import yaml
 
 app = Flask(__name__)
 app.debug = True
 sockets = Sockets(app)
 
+test_message = [
+    [{"text":"while","flag": True}, {"text":"the","flag": False}, {"text":"final","flag": False}, {"text":"say","flag": False}],
+    [{"text":"crazed","flag": False}, {"text":"monosyllables","flag": False}],
+    [{"text":"modesto","flag": False}, {"text":"shallow","flag": False}]
+]
 
 @app.route('/')
 def main():
@@ -18,12 +25,16 @@ def candidates():
     return 'hi'
 
 
-@sockets.route('/echo')
+@sockets.route('/twaiku')
 def echo_socket(ws):
     while not ws.closed:
         message = ws.receive()
-        ws.send("fuck this shit")
-
+        message = yaml.safe_load(message)
+        print message
+        if (message["event"] == 'haiku'):
+            ws.send(json.dumps(test_message))
+        else:
+            ws.send("fuck this shit")
 
 if __name__ == '__main__':
     server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
