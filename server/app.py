57affc648bbac0171aad2822d25e5d227a8a88c1
Edwin Gomez Cuellar <nickgomez@Edwins-MacBook-Pro.local>
2016-02-07 01:44:55 -0500

    fuck this shit

--- server/app.py
+++ server/app.py
@@ -1,14 +1,30 @@
 from flask import Flask
+from flask_sockets import Sockets
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
 
 app = Flask(__name__)
+app.debug = True
+sockets = Sockets(app)
+
 
 @app.route('/')
 def main():
-	return "Hello World"
+    return "Hello World"
+
 
 @app.route('/candidates')
 def candidates():
-	return 'hi'
+    return 'hi'
+
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send("fuck this shit")
+
 
 if __name__ == '__main__':
-	app.run()
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
