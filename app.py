b5a3c62197aede11955faab966a13c51e2745ff3
rtran9 <rtranare@gmail.com>
2016-02-07 04:30:44 -0500

    moved to root

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def echo_socket(ws):
             ws.send("fuck this shit")
 
 if __name__ == '__main__':
-    server = pywsgi.WSGIServer(('', 80), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
     server.serve_forever()
